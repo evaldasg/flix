7367e64f936c91529d5ca1ce072b9e5bcb3be9f0
Evaldas Grudzinskas <evaldas.g@icloud.com>
2014-03-02 23:56:40 +0200

    Created form partial, edit.html, new.html, create_movie_spec, delete_movie_spec, edit_movie_spec

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,36 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+
+    redirect_to @movie
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+
+private
+  def movie_params
+    params[:movie].permit(:title, :description, :rating, :released_on, :total_gross)
+  end
 end
---
+++ spec/features/create_movie_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "Creating a new movie" do
+
+  it "saves the movie and shows the new movie's details" do
+    visit movies_url
+    click_link 'Add New Movie'
+
+    expect(current_path).to eq(new_movie_path)
+
+    fill_in 'Title', with: 'New Title'
+    fill_in 'Description', with: 'Super great film and very fun of course'
+    fill_in 'Rating', with: "PG"
+    fill_in 'Total gross', with: '75000000'
+    select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
+
+    click_button 'Create Movie'
+
+    expect(current_path).to eq(movie_path(Movie.last))
+
+    expect(page).to have_text('New Title')
+  end
+end
