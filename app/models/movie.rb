ba15407409ee806c37d13816c813eac679d5e657
Evaldas Grudzinskas <evaldas.g@icloud.com>
2014-03-03 16:05:24 +0200

    Created error messgaes when submiting invalid movie form

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,12 +3,13 @@ class Movie < ActiveRecord::Base
   validates :title, :released_on, :duration, presence: true
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
-  validates :duration, numericality: { only_integer: true }, length: { maximum: 3 }
+  validates :duration, numericality: { only_integer: true }, length: { maximum: 3,
+            message: "max 3 characters allowewd" }
   validates :image_file_name, allow_blank: true, format: {
     with: /\w+.(gif|jpg|png)\z/i,
     message: "must reference a GIF, JPG, or PNG image"
   }
-  validates :rating, inclusion: { in: RATINGS }
+  validates :rating, inclusion: { in: RATINGS, message: "is not selected" }
 
   def flop?
     total_gross.blank? || total_gross < 50000000
